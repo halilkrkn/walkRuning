5b2794f2a25835124c3ae580df90e869f035d4be
Halilkrkn <halilkrkn000@gmail.com>
2021-06-19 21:31:36 +0300

    Tracking Service Kurulumu Yapıldı

--- app/src/main/java/com/example/walkruning/ui/fragments/TrackingFragment.kt
+++ app/src/main/java/com/example/walkruning/ui/fragments/TrackingFragment.kt
@@ -1,10 +1,14 @@
 package com.example.walkruning.ui.fragments
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import com.example.walkruning.R
+import com.example.walkruning.other.Constants
+import com.example.walkruning.other.Constants.ACTION_START_OR_RESUME_SERVICE
+import com.example.walkruning.service.TrackingService
 import com.example.walkruning.ui.viewmodels.MainViewModel
 import com.google.android.gms.maps.GoogleMap
 import dagger.hilt.android.AndroidEntryPoint
@@ -20,9 +24,20 @@ class TrackingFragment:Fragment(R.layout.fragment_tracking) {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        btnToggleRun.setOnClickListener {
+            sendCommmandToService(ACTION_START_OR_RESUME_SERVICE)
+        }
+
         mapView.onCreate(savedInstanceState)
         mapView.getMapAsync {
             map = it
+        }
+    }
+
+    private fun sendCommmandToService(action:String){
+        Intent(requireContext(),TrackingService::class.java).also {
+            it.action = action
+            requireContext().startService(it)
         }
     }
 
