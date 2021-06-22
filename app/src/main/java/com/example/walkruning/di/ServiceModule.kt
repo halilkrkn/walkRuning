71272c0d40797ce78ef00aa6dcba9e5471c10cad
Halilkrkn <halilkrkn000@gmail.com>
2021-06-22 14:47:02 +0300

    Kronometre sayacının Bildirim çubuğunda da kontrol edilmesi ve gösterilmesi

---
+++ app/src/main/java/com/example/walkruning/di/ServiceModule.kt
@@ -0,0 +1,64 @@
+package com.example.walkruning.di
+
+import android.app.PendingIntent
+import android.content.Context
+import android.content.Intent
+import androidx.core.app.NotificationCompat
+import com.example.walkruning.R
+import com.example.walkruning.other.Constants
+import com.example.walkruning.ui.MainActivity
+import com.google.android.gms.location.FusedLocationProviderClient
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.components.ServiceComponent
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.android.scopes.ServiceScoped
+
+
+// TODO: 22.06.2021 Notification (Bildirim) Çubuğunda kronometre sayacının aktifleştirilmesi ve bildirim çubuğu üzerinden sayacın kontrolü yapıldı. 
+@Module
+@InstallIn(ServiceComponent::class)
+object ServiceModule {
+
+    @ServiceScoped
+    @Provides
+    fun provideFusedLocationProviderClient(
+            @ApplicationContext app: Context
+    ) = FusedLocationProviderClient(app)
+
+
+
+    // app içerisinde tracking fragment içerisindeki harita çalıştırıldığında main activite bunu alıcak sonrasında bildirim çubuğunda gözükmesi sağlanacak
+    @ServiceScoped
+    @Provides
+    fun provideMainActivityPendingIntent(
+            @ApplicationContext app: Context
+    ) =  PendingIntent.getActivity(
+            app,
+            0,
+            Intent(app, MainActivity::class.java).also {
+                it.action = Constants.ACTION_SHOW_TRACKING_FRAGMENT
+            },
+            PendingIntent.FLAG_UPDATE_CURRENT
+    )
+
+
+    //Notification İşlemleri
+    // App içerisindeki haritayı çalıştırdıktan sonra Cihazın bildirim çubuğunda  oluşan uygulama işlemleri kısmı
+    // Yani bildirim çubuğunda olması gereken işlemleri inşaa ettik.
+    @ServiceScoped
+    @Provides
+    fun provideBaseNotificationBuilder(
+            @ApplicationContext app: Context,
+            pendingIntent: PendingIntent
+    ) =  NotificationCompat.Builder(app, Constants.NOTIFICATION_CHANEL_ID)
+            .setAutoCancel(false)
+            .setOngoing(true)
+            .setSmallIcon(R.drawable.ic_baseline_directions_run_24)
+            .setContentTitle("walkRunning")
+            .setContentText("00:00:00")
+            .setContentIntent(pendingIntent)
+
+
+}
--- app/src/main/java/com/example/walkruning/service/TrackingService.kt
+++ app/src/main/java/com/example/walkruning/service/TrackingService.kt
@@ -6,6 +6,7 @@ import android.app.NotificationManager
 import android.app.NotificationManager.IMPORTANCE_LOW
 import android.app.PendingIntent
 import android.app.PendingIntent.FLAG_UPDATE_CURRENT
+import android.content.Context
 import android.content.Intent
 import android.location.Location
 import android.os.Build
@@ -34,21 +35,33 @@ import com.google.android.gms.location.LocationRequest
 import com.google.android.gms.location.LocationRequest.PRIORITY_HIGH_ACCURACY
 import com.google.android.gms.location.LocationResult
 import com.google.android.gms.maps.model.LatLng
+import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 import timber.log.Timber
+import javax.inject.Inject
 
 
 typealias Polyline = MutableList<LatLng>
 typealias Polylines = MutableList<Polyline>
+
+@AndroidEntryPoint
 class TrackingService: LifecycleService() {
 
     var isFirstRun = true
 
+
+
     // Konum Sağlayıcısı
+    @Inject
     lateinit var  fusedLocationProviderClient: FusedLocationProviderClient
+
+    @Inject
+    lateinit var baseNotificationBuilder: NotificationCompat.Builder
+
+    lateinit var currentNotificationBuilder: NotificationCompat.Builder
 
     private val timeRunInSeconds = MutableLiveData<Long>()
 
@@ -70,11 +83,13 @@ class TrackingService: LifecycleService() {
     // Location Sağlayıcı biligilerini Bilgileri ve TrackingFragment de çalışması için
     override fun onCreate() {
         super.onCreate()
+        currentNotificationBuilder = baseNotificationBuilder
         postInitialValues()
         fusedLocationProviderClient = FusedLocationProviderClient(this)
 
         isTracking.observe(this, Observer {
             updateLocationTracking(it)
+            updateNotificationTrackingState(it)
         })
 
 
@@ -111,6 +126,33 @@ class TrackingService: LifecycleService() {
         isTracking.postValue(false)
         isTimerEnabled = false
     }
+
+    private fun updateNotificationTrackingState(isTracking: Boolean){
+        val notificationActionText = if (isTracking) "Pause" else "Resume"
+
+        val pendingIntent = if (isTracking){
+            val pauseIntent = Intent(this, TrackingService::class.java).apply {
+                action = ACTION_PAUSE_SERVICE
+            }
+            PendingIntent.getService(this,1,pauseIntent, FLAG_UPDATE_CURRENT)
+        } else {
+            val resumeIntent = Intent(this, TrackingService::class.java).apply {
+                action = ACTION_START_OR_RESUME_SERVICE
+            }
+            PendingIntent.getService(this,2,resumeIntent, FLAG_UPDATE_CURRENT)
+        }
+
+        val notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+
+        currentNotificationBuilder.javaClass.getDeclaredField("mActions").apply {
+            isAccessible = true
+            set(currentNotificationBuilder,ArrayList<NotificationCompat.Action>())
+        }
+        currentNotificationBuilder = baseNotificationBuilder
+                .addAction(R.drawable.ic_baseline_motion_photos_paused_24, notificationActionText,pendingIntent)
+        notificationManager.notify(NOTIFICATION_ID,currentNotificationBuilder.build())
+    }
+
 
     // TODO: 21.06.2021 ****** KRONOMETRE ISLEMLERI **********
     // TrackingUtility de ise Kronometre kurulum işlemleri yapıldı.
@@ -161,32 +203,14 @@ class TrackingService: LifecycleService() {
             createNotificationChannel(notificationManager)
         }
 
-    //Notification İşlemleri
-    // App içerisindeki haritayı çalıştırdıktan sonra Cihazın bildirim çubuğunda  oluşan uygulama işlemleri kısmı
-    // Yani bildirim çubuğunda olması gereken işlemleri inşaa ettik.
-    val notificationBuilder =  NotificationCompat.Builder(this, NOTIFICATION_CHANEL_ID)
-            .setAutoCancel(false)
-            .setOngoing(true)
-            .setSmallIcon(R.drawable.ic_baseline_directions_run_24)
-            .setContentTitle("walkRunning")
-            .setContentText("00:00:00")
-            .setContentIntent(getMainActivityPendingIntent())
+    startForeground(NOTIFICATION_ID,baseNotificationBuilder.build())
 
-
-    startForeground(NOTIFICATION_ID,notificationBuilder.build())
+    timeRunInSeconds.observe(this, Observer {
+        val notification = currentNotificationBuilder
+                .setContentText(TrackingUtility.getFormattedStopWatchTime(it * 1000L))
+        notificationManager.notify(NOTIFICATION_ID, notification.build())
+    })
     }
-
-
-    // app içerisinde tracking fragment içerisindeki harita çalıştırıldığında main activite bunu alıcak sonrasında bildirim çubuğunda gözükmesi sağlanacak
-    private fun getMainActivityPendingIntent() = PendingIntent.getActivity(
-            this,
-            0,
-            Intent(this, MainActivity::class.java).also {
-                it.action = ACTION_SHOW_TRACKING_FRAGMENT
-            },
-            FLAG_UPDATE_CURRENT
-    )
-
 
 
     //Notification (Bildirim Çubuğu) Oluşturuyoruz.
