4e2f1c5d30a0ad7672ed9e1b824fcff2f702584b
Halilkrkn <halilkrkn000@gmail.com>
2021-06-24 18:13:32 +0300

    Kullanıcı İsmi ve Ağırlığının kullanıcı tarafından alınması

--- app/src/main/java/com/example/walkruning/di/AppModule.kt
+++ app/src/main/java/com/example/walkruning/di/AppModule.kt
@@ -1,8 +1,14 @@
 package com.example.walkruning.di
 import android.content.Context
+import android.content.Context.MODE_PRIVATE
+import android.content.SharedPreferences
 import androidx.room.Room
 import com.example.walkruning.db.RunningDatabase
+import com.example.walkruning.other.Constants.KEY_FIRST_TIME_TOGGLE
+import com.example.walkruning.other.Constants.KEY_NAME
+import com.example.walkruning.other.Constants.KEY_WEIGHT
 import com.example.walkruning.other.Constants.RUNNING_DATABASE_NAME
+import com.example.walkruning.other.Constants.SHARED_PREFERENCES_NAME
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -29,4 +35,36 @@ object AppModule {
     @Singleton
     @Provides
     fun provideRunDao(db:RunningDatabase) = db.getRunningDao()
+
+    // TODO: 24.06.2021
+    //  Kullanıcı İsim ve Ağırlığını almak için sharedReferences fonk. yararlanıp telefonun hafızasında kayıt etmiş olduk.
+    @Singleton
+    @Provides
+    fun provideSharedPreferences(@ApplicationContext app: Context) =
+        app.getSharedPreferences(SHARED_PREFERENCES_NAME, MODE_PRIVATE)
+
+    // Bu fonk. yapısı ile yukarıdaki fonksiyonun yapısı aynıdır.
+    // Yukarıdaki fonk. Single Expression Functions özelliğini kullanıyor. Arasındaki fark ise " = " operatörü sayesinde sadece bir işlemlik görevleri yapılıyor ve Derleyi hangi dönüş tipinde olduğunu otomatik kendisi  algılıyor ve " = " operatörü  de dönüş tipine göre return yapıyor. Yani "=" operatörü return işleminin yerine geçmiş oluyor.
+    // Bu yüzden kod fazlalığından kurtulmuş ve okunurluluk artmış oluyor.
+
+//    fun provideSharedPreferences(@ApplicationContext app: Context): SharedPreferences{
+//        return app.getSharedPreferences(SHARED_PREFERENCES_NAME, MODE_PRIVATE)
+//
+//    }
+
+    @Singleton
+    @Provides
+    fun provideName(sharedPref: SharedPreferences): String {
+       return sharedPref.getString(KEY_NAME, "") ?: ""
+    }
+
+
+    @Singleton
+    @Provides
+    fun provideWeight(sharedPref: SharedPreferences) = sharedPref.getFloat(KEY_WEIGHT, 80f)
+
+
+    @Singleton
+    @Provides
+    fun provideFirstTimeToggle(sharedPref: SharedPreferences) = sharedPref.getBoolean(KEY_FIRST_TIME_TOGGLE, true)
 }
--- app/src/main/java/com/example/walkruning/ui/fragments/SetupFragment.kt
+++ app/src/main/java/com/example/walkruning/ui/fragments/SetupFragment.kt
@@ -1,19 +1,74 @@
 package com.example.walkruning.ui.fragments
 
+import android.content.SharedPreferences
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.navigation.NavOptions
 import androidx.navigation.fragment.findNavController
 import com.example.walkruning.R
+import com.example.walkruning.other.Constants.KEY_FIRST_TIME_TOGGLE
+import com.example.walkruning.other.Constants.KEY_NAME
+import com.example.walkruning.other.Constants.KEY_WEIGHT
+import com.google.android.material.snackbar.Snackbar
+import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.android.synthetic.main.fragment_setup.*
+import javax.inject.Inject
 
+// TODO: 24.06.2021 Kullanıcı İsmi ve Ağırlığının kullanıcı tarafından alınması ile başka bir fragmentta yönlendirilmesi
+@AndroidEntryPoint
 class SetupFragment:Fragment(R.layout.fragment_setup) {
+
+    @Inject
+    lateinit var sharedPref : SharedPreferences
+
+    @set:Inject
+    var isFirstAppOpen = true
+
+
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        tvContinue.setOnClickListener {
-            findNavController().navigate(R.id.action_setupFragment_to_runningFragment)
+
+        // Uygulama ilk Açıldığında UI lar yani fragmentlar arasında gönderme yapılıyor.
+        if (!isFirstAppOpen){
+            val navOptions = NavOptions.Builder()
+                    .setPopUpTo(R.id.setupFragment,true)
+                    .build()
+            findNavController().navigate(
+                    R.id.action_setupFragment_to_runningFragment,
+                    savedInstanceState,
+                    navOptions
+            )
         }
+
+
+        // Uygulama içersinde setup fragmenttan runningFragmenta yönlendirme işlemi yapılıyor ve editText lerde kullanıcının bilgilerinin girilmesi isteniyor.
+        tvContinue.setOnClickListener {
+            val success = writePersonalDataToSharedPref()
+            if (success){
+                findNavController().navigate(R.id.action_setupFragment_to_runningFragment)
+            } else {
+                Snackbar.make(requireView(),"Please enter all the fields", Snackbar.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+    // BU kısımda ise kullanıcının setupfragmentta yani uygulama açıldığındaki ilk Uı daki gerekli kısımların doldurulması ve girilen bilgilerin sharedPreferences vasıtasıyla uygulama hafızasına eklenmesi işlemleri.
+    private fun writePersonalDataToSharedPref():Boolean{
+        val name = etName.text.toString()
+        val weight = etWeight.text.toString()
+        if (name.isEmpty() || weight.isEmpty()){
+            return false
+        }
+        sharedPref.edit()
+                .putString(KEY_NAME,name)
+                .putFloat(KEY_WEIGHT,weight.toFloat())
+                .putBoolean(KEY_FIRST_TIME_TOGGLE,false)
+                .apply()
+//        val toolbarText = "Let's go, $name!"
+//        requireActivity().tvToolbarTitle.text = toolbarText
+        return true
     }
 }
